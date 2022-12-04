da927b4dcccf586b21ad1ed1fab74f9c8e3ec5a4
nayara dias <nayaradias743@gmail.com>
2022-12-04 12:28:23 -0300

    adjust imports

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+export { default as  Avatar } from './Avatar';
+export { default as  Button } from './Button';
+export { default as  ButtonCounter } from './ButtonCounter';
+export { default as  CheckRadio } from './CheckRadio';
+export { default as  Input } from './Input';
+export { default as  InputRange } from './InputRange';
